068053c209d34258bc1d90f4784e06540124ff76
Matthew Toy <matthewtoy@gmail.com>
2018-11-06 22:23:39 +0000

    Notes change

--- WpfTest/Repository.cs
+++ WpfTest/Repository.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace WpfTest
